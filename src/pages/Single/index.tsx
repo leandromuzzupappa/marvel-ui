0713701c5c5c20e63a86fa8ca418ddaafe396536
lmuzzupappa <lmuzzupappa@glamit.com.ar>
2022-02-18 17:57:25 -0300

    Single page started, also created fetch single service

--- src/pages/Single/index.tsx
+++ src/pages/Single/index.tsx
@@ -1,9 +1,30 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router';
+import { useParams } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import { fetchSingle } from 'services/marvelService';
+
 const Single = () => {
+  const { id } = useParams();
+  const { pathname } = useLocation();
+  const currentPage = pathname?.split('/')[1];
+
+  const { data: characterData } = useQuery(
+    [`marvel-single-${currentPage}-data${id}`, { fetchType: currentPage, id }],
+    fetchSingle,
+    {
+      keepPreviousData: true,
+    },
+  );
+
+  useEffect(() => {
+    console.log(characterData);
+  }, [characterData]);
+
   return (
-    <>
-      <h1>Single</h1>
-      <h2>23124132</h2>
-    </>
+    <div style={{ minHeight: '50vh' }}>
+      <h2>{characterData?.results.name}</h2>
+    </div>
   );
 };
 
--- src/services/marvelService.ts
+++ src/services/marvelService.ts
@@ -28,3 +28,21 @@ export const fetchCaracters = async ({ queryKey }: queryKeyType) => {
     statusCode: res.data.code,
   };
 };
+
+export const fetchSingle = async ({ queryKey }: queryKeyType) => {
+  const { fetchType, id } = queryKey[1];
+
+  const res = await axios.get(
+    `https://gateway.marvel.com:443/v1/public/${fetchType}/${id}`,
+    {
+      params: {
+        ...MARVEL_KEYS,
+      },
+    },
+  );
+  return {
+    data: res.data.data,
+    results: res.data.data.results[0],
+    statusCode: res.data.code,
+  };
+};
